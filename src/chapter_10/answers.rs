fd6d53b34a2619cc1bfa934bbb0ac292ce119063
rabbit <barateligeorge@yandex.ru>
2026-06-17 08:46:27 +0300

    chapter 10 task 2

--- src/chapter_10/answers.rs
+++ src/chapter_10/answers.rs
@@ -3,7 +3,7 @@ fn largest<T: PartialOrd>(list: &[T]) -> Option<&T> {
         return None;
     }
 
-    let mut max  = &list[0];
+    let mut max = &list[0];
 
     for item in list {
         if item > max {
@@ -15,15 +15,15 @@ fn largest<T: PartialOrd>(list: &[T]) -> Option<&T> {
 }
 
 pub fn check_1() {
-    let array: Vec<i32> = vec!(1, 2, 5, 123, 43, 14);
+    let array: Vec<i32> = vec![1, 2, 5, 123, 43, 14];
     let result = largest(&array);
 
     match result {
         Some(item) => println!("largest i32 = {item}"),
         None => (),
     }
-    
-    let array: Vec<char> = vec!('a', 'b', 'c', 'd');
+
+    let array: Vec<char> = vec!['a', 'b', 'c', 'd'];
     let result = largest(&array);
 
     match result {
@@ -31,7 +31,7 @@ pub fn check_1() {
         None => (),
     }
 
-    let array: Vec<f64> = vec!(4.2, 6.5, 11.4, 3.6);
+    let array: Vec<f64> = vec![4.2, 6.5, 11.4, 3.6];
     let result = largest(&array);
 
     match result {
@@ -40,63 +40,67 @@ pub fn check_1() {
     }
 }
 
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn new(x: T, y: U) -> Self {
+        Self { x, y }
+    }
+
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+        &self.y
+    }
+
+    fn swap(self) -> Point<U, T> {
+        Point {
+            x: self.y,
+            y: self.x,
+        }
+    }
+}
 
 pub fn check_2() {
+    let point = Point::new(1, 22);
+    println!("x: {}, y: {}", point.x(), point.y());
+    let point = point.swap();
+    println!("After swap => x: {}, y: {}", point.x(), point.y());
 
+    let point = Point::new(1, 22.5);
+    println!("x: {}, y: {}", point.x(), point.y());
+    let point = point.swap();
+    println!("After swap => x: {}, y: {}", point.x(), point.y());
+
+    let point = Point::new("Hello", String::from(", George"));
+    println!("x: {}, y: {}", point.x(), point.y());
+    let point = point.swap();
+    println!("After swap => x: {}, y: {}", point.x(), point.y());
 }
 
+pub fn check_3() {}
 
-pub fn check_3() {
+pub fn check_4() {}
 
-}
+pub fn check_5() {}
 
+pub fn check_6() {}
 
-pub fn check_4() {
+pub fn check_7() {}
 
-}
+pub fn check_8() {}
 
+pub fn check_9() {}
 
-pub fn check_5() {
+pub fn check_10() {}
 
-}
+pub fn check_11() {}
 
+pub fn check_12() {}
 
-pub fn check_6() {
-
-}
-
-
-pub fn check_7() {
-
-}
-
-
-pub fn check_8() {
-
-}
-
-
-pub fn check_9() {
-
-}
-
-
-pub fn check_10() {
-
-}
-
-
-pub fn check_11() {
-
-}
-
-
-pub fn check_12() {
-
-}
-
-
-pub fn check_13() {
-
-}
-
+pub fn check_13() {}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod chapter_10;
 
 fn main() {
-    chapter_10::answers::check_1();
+    chapter_10::answers::check_2();
 }
